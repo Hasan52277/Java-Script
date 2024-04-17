5fbaebb24277e3a81eff34fc8deabe07cfe58bae
Hasan <mohdhasan52277@gmail.com>
2024-04-17 15:05:09 +0530

    loop

---
+++ 26_Control-flow.js
@@ -0,0 +1,61 @@
+// if Statements
+
+// const isuserloggedin = true
+// const temperature = 41
+
+// if (temperature<50) {
+//     console.log("less than 50");
+    
+// }
+// else{
+//     console.log("greater than 50");  
+// }
+// console.log("greater than 50");
+
+//  < , > ,<=, >=,==, != , === , !==
+
+
+
+// const score = 200
+// if (score>100) {
+//     const power = "fly"
+//     console.log(`user power : ${power}`);
+    
+// }
+// console.log(`user power : ${power}`);
+
+
+
+// const balance = 1000
+
+// // if (balance>500) console.log("text");
+
+// if (balance<500) {
+//     console.log("less than 500");
+    
+// }
+// else if (balance<700) {
+//     console.log("less than 700");
+// } 
+// else if(balance<900){
+// console.log("less than 900");
+// }
+// else{
+//     console.log("less than 9");
+// }
+
+
+
+
+const userLoggedIn =true
+const debitcards =true
+const LoggedInfromgoogle = false
+const LoggedInfr
+
+if (userLoggedIn && debitcards) {
+    console.log("allow to buy course");
+    
+}
+if () {
+    
+}
