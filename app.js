54641ffeeeb3448ae4ec811b935cf16f20e9cb53
Hasan <mohdhasan52277@gmail.com>
2024-07-22 10:13:15 +0530

    Timeout

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-// DOM
+const name  = "Hasan"
+const num = 29
 
-console.log(window.document);
+// console.log(`My name is ${name} and My age is ${num}`);
 
-const mainHeading  = document.getElementById()
+
+const newname  = new String(" Hasan ")
+console.log(newname);
+
+// console.log(newname.fontsize())
+// console.log(newname.charAt("3"));
+// console.log(newname.substring(1,4));
+console.log(newname.trim());
