392172ca85ff669043bcc4a0eec683b3473d6d3f
Hasan <mohdhasan52277@gmail.com>
2024-07-23 21:17:27 +0530

    Prectice

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
-function loginUserMessage(username){
-    if (username===undefined) {
-        console.log("pleace enter user name");
-        return
-    }
-    return `${username} just logged in`
-}
-console.log(loginUserMessage("Hasan"))
-console.log(loginUserMessage())
+// promise
+
+// const promiseOne = new Promise(function (resolve,reject) {
+//     setTimeout(function () {
+//         console.log("asyn task is completed");
+//         resolve()
+//     },1000)
+// })
+// promiseOne.then(function () {
+//     console.log("promise consumed");
+// })
+
+
+// 3.Method of promise
+
+// const  promiseThree = new Promise(function (resolve,reject) {
+//     setTimeout(function () {
+//         resolve({name:"Hasan",age:"20"})
+//     },1000)
+// })
+// .then(function (user) {
+//     console.log(user);
+    
+// })
+
+// 4.. Method of promise
+
+const promiseFour = new Promise(function (resolve,reject) {
+    setTimeout(function () {
+        let error = true
+        if (!true) {
+            resolve({myname:"Hasan",age:18})
+            
+        }
+        else{
+            resolve({friendname:"junaid",friendage:18})
+        }
+    },1000)
+}) 
+promiseFour.then(function (user) {
+    console.log(user);
+    return user.myname
+    
+})
