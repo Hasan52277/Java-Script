e62883e2d8a3e6132c423d073870da2184d1fba4
Hasan <mohdhasan52277@gmail.com>
2024-07-27 16:05:09 +0530

    new

--- app.js
+++ app.js
@@ -1,67 +1,11 @@
-// let myName = "hitesh     "
-// let mychannel = "chai     "
+// for loop 
 
-// console.log(myName.trueLength);
-
-
-let myHeros = ["thor", "spiderman"]
-
-
-let heroPower = {
-    thor: "hammer",
-    spiderman: "sling",
-
-    getSpiderPower: function(){
-        console.log(`Spidy power is ${this.spiderman}`); // this ka matrlab ise ka 
+for(let i =0; i<=10; i++){
+    const Element  =i
+    if (Element == 5) {
+        console.log("5 is a best Num");
+        break
     }
+    console.log(i);
+   
 }
-
-Object.prototype.hitesh = function(){
-    console.log(`hitesh is present in all objects`);
-}
-
-Array.prototype.heyHitesh = function(){
-    console.log(`Hitesh says hello`);
-}
-
-// heroPower.hitesh()
-// myHeros.hitesh()
-// myHeros.heyHitesh()
-// heroPower.heyHitesh()
-
-// inheritance
-
-const User = {
-    name: "chai",
-    email: "chai@google.com"
-}
-
-const Teacher = {
-    makeVideo: true
-}
-
-const TeachingSupport = {
-    isAvailable: false
-}
-
-const TASupport = {
-    makeAssignment: 'JS assignment',
-    fullTime: true,
-    __proto__: TeachingSupport
-}
-
-Teacher.__proto__ = User
-
-// modern syntax
-Object.setPrototypeOf(TeachingSupport, Teacher)
-
-let anotherUsername = "ChaiAurCode     "
-
-String.prototype.trueLength = function(){
-    console.log(`${this}`);
-    console.log(`True length is: ${this.trim().length}`);
-}
-
-anotherUsername.trueLength()
-"hitesh".trueLength()
-"iceTea".trueLength()
